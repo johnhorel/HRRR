ab750ab5deb957a394d64a97a64eefd7954cb525
00845413 <u0845413@utah.edu>
2020-07-31 11:00:45 -0600

    Cleaned up code base and added functionality to merge in other directory of files
    Merge branch 'master' of https://github.com/johnhorel/HRRR
    
    # Conflicts:
    #	rclone_scripts/rclone_auto.sh

--- AWS_migration/ssh_script_new.py
+++ AWS_migration/ssh_script_new.py
@@ -69,36 +69,6 @@ hostname = 'kingspeak.chpc.utah.edu'
 path = '/uufs/chpc.utah.edu/common/home/horel-group7/Pando/hrrr'
 port = 22
 
-'''
-No longer needed (keep code in case cached credentials are needed later)
-# Pull in user information from external source
-arr = []
-with open("user_info.txt", "r+") as f:
-    for line in f:
-        arr.append(line)
-        
-val1 = arr[0].split(" = ")
-val2 = arr[1].split(" = ")
-
-# Populate to user fields
-username = val1[1]
-password = val2[1]
-
-# Declare parameters for AWS S3 connectivity
-# Pull in keys from external source
-arr2 = []
-with open("AWS.txt", "r+") as f:
-    for line in f:
-        arr2.append(line)
-        
-val1 = arr2[0].split(" = ")
-val2 = arr2[1].split(" = ")
-
-# Populate to key fields
-ACCESS_KEY = val1[1]
-SECRET_KEY = val2[1]
-'''
-
 # Access Pando
 fs = s3fs.S3FileSystem(anon=True, client_kwargs={'endpoint_url':"https://pando-rgw01.chpc.utah.edu/"})
 # The ls command can be slow, so we can force the read_timeout to be patient
@@ -110,6 +80,7 @@ os.mkdir("Staging")
 # Need to create file structure similar to Pando and copy data files into it
 # Start with sfc files
 # Let x = 1st day data was collected
+# All data is archived back to the same date
 x = 20160715 
 # Compare to day program is run to prevent missing any days
 today = datetime.today().strftime('%Y%m%d')
@@ -117,12 +88,14 @@ t = int(today)
 print("Staging files...")
 os.mkdir("Staging/sfc")
 while x < t:
+    # Make sfc dir
     os.mkdir("Staging/sfc/" + str(x))
+    # Make prs dir
+    os.mkdir("Staging/prs" + str(x))
     yr = str(x)[0:4]
     dest = str(x)
     # Start new year
     if str(x).endswith('1231'):
-
         x += 8869
     # 31 day months
     if str(x).endswith('31'):
@@ -133,84 +106,37 @@ while x < t:
     # February non-leap years
     if str(x).endswith('0228') and int(yr) % 4 != 0:
         x += 72
+    # Account for 00 year non-leap yrs (not divisible by 400)
+    if yr.endswith('00') and int(yr) % 400 != 0:
+        x +=72
     # February leap years
     if str(x).endswith('0229') and int(yr) % 4 == 0:
         x += 71
     else:
         x += 1
     try:
+         # Run one at a time
          files = fs.ls('hrrr/sfc/' + str(x))[-3:]
+         #files2 = fs.ls('hrrr/prs/' + str(x))[-3:]
     # Catch in case counter tries to pull non-existant day
     except FileNotFoundError:
         continue
-    # Import files
+    # Import sfc files
     for file in files:
+    #for file in files2:
         item = str(file)
         lst = item.split("/")
         idx = len(lst) - 1
         name = lst[idx]
         path = "Staging/sfc/" + dest + "/" + name
+        #path = "Staging/prs/" + dest + "/" + name
         fs.download(file, path)
 
 # Upload files from local dir using rclone
 print("Uploading to AWS...")
 base = os.getcwd()
-result = rclone.with_config(cfg).run_cmd(command="sync", extra_args=["/base/Staging/sfc", "AWS test:transferfrompando/sfc"])
-
-# Define function that uploads to AWS via Boto3
-'''
-Delete in next release
-def upload_to_aws(local_file, bucket, s3_file=None):
-    s3 = boto3.client('s3')
-                      #, aws_access_key_id = ACCESS_KEY,
-                      #aws_secret_access_key = SECRET_KEY)
-
-     # If S3 object_name was not specified, use file_name
-    if s3_file is None:
-        s3_file = local_file
-        
-    try:
-        s3.upload_file(local_file, bucket, s3_file)
-        print("Upload Successful")
-        return True
-    except FileNotFoundError:
-        print("The file was not found")
-        return False
-    # This exception prints if the s3 arguments do not agree with the bucket parameters
-    except HTTPClientError as ex:
-        print(ex)
-        return False
-    except NoCredentialsError:
-        print("Credentials not available")
-        return False
-    
-# Create function to check for duplicates
-def check_duplicates(bucket, key_file, objs):
-    if len(objs) > 0 and objs[0].key == key_file:
-        s3 = boto3.client('s3')
-        s3.delete_object(Bucket = 'transferfrompando', Key = str(key_file))
-    else:
-        pass
-       
-# Perform duplicate check
-sx = boto3.resource('s3')
-bucket = sx.Bucket('transferfrompando')
-
-# Perform file upload to AWS
-contents = os.listdir("Staging")
-i = 0
-print("Uploading files to AWS...")
-os.chdir("Staging")
-for item in contents:
-    # Before uploading, make sure it doesn't already exist in bucket
-    key_file = item
-    objs = list(bucket.objects.filter(Prefix=key_file))
-    duplicate = check_duplicates(bucket, key_file, objs)
-    #Upload files
-    uploaded = upload_to_aws(key_file, 'transferfrompando')
-                             #, 'HRRR_Archive')
-    i += 1
-'''
+result = rclone.with_config(cfg).run_cmd(command="sync", extra_args=[base + "/Staging/sfc", "AWS test:transferfrompando/sfc"]) 
+#result = rclone.with_config(cfg).run_cmd(command="sync", extra_args=[base + "/Staging/prs", "AWS test:transferfrompando/prs"]) 
 
 # Change dir back to working directory   
 os.chdir("..")
